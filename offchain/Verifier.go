87dc74a3a0ff21f044783d02adef60b2736f5318
wanghl21 <50040344+wanghl21@users.noreply.github.com>
2022-03-12 22:09:01 +0800

    Update Verifier.go

--- offchain/Verifier.go
+++ offchain/Verifier.go
@@ -1,7 +1,10 @@
 package offchain
 
 import (
+	"encoding/json"
 	"fmt"
+
+	"github.com/W1llyu/ourjson"
 )
 
 type Verifier struct {
@@ -10,6 +13,56 @@ type Verifier struct {
 
 func NewVerifier() Verifier {
 	return Verifier{NewPublicParam()}
+
+}
+
+func (vr *Verifier) getChallenges(verifier Verifier, challengesObject []byte) []Challenge {
+	//chalstring := Utils{}.readFile("../config/challenges")
+	fmt.Print("Blockchain getChallenges......\t\t\t")
+	var challenges []Challenge
+	var cmap map[int]string
+	err := json.Unmarshal(challengesObject, &cmap)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for k, v := range cmap {
+		keyInt := k
+		valueBytes := Utils{}.base64StringToElementBytes(v)
+		valueElement := verifier.keys.pairing.NewZr().SetBytes(valueBytes)
+		challenges = append(challenges, Challenge{num: keyInt, random: *valueElement})
+	}
+	return challenges
+
+}
+
+func (vr *Verifier) getProofObeject(verifier Verifier, proofobject []byte) ProofObject {
+	fmt.Print("Blockchain getProofObeject......\t\t\t")
+	//proofString := Utils{}.readFile("../config/proof")
+	proofObject, _ := ourjson.ParseObject(string(proofobject))
+
+	miuString, _ := proofObject.GetString("miu")
+	miu := verifier.keys.pairing.NewZr().SetBytes(Utils{}.base64StringToElementBytes(miuString))
+	wString, _ := proofObject.GetString("R")
+	r := verifier.keys.pairing.NewG2().SetBytes(Utils{}.base64StringToElementBytes(wString))
+
+	hashMulString, _ := proofObject.GetString("hashMul")
+	hashMul := verifier.keys.pairing.NewG1().SetBytes(Utils{}.base64StringToElementBytes(hashMulString))
+	proof := Proof{
+		Miu:     *miu,
+		R:       *r,
+		HashMul: *hashMul,
+	}
+
+	miuob := Utils{}.elementToBase64(proof.Miu)
+	rob := Utils{}.elementToBase64(proof.R)
+	hashMulob := Utils{}.elementToBase64(proof.HashMul)
+	var proofOb = ProofObject{
+		Miu:     miuob,
+		R:       rob,
+		HashMul: hashMulob,
+	}
+	return proofOb
 
 }
 
